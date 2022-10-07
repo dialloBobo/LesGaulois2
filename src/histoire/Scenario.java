798af7ed85c96ba54ea18e63e726b1a983ac7dc8
Mariama DIALLO <diallo.mbobo108@gmail.com>
2022-10-07 22:29:17 +0200

    le village gaulois

--- src/histoire/Scenario.java
+++ src/histoire/Scenario.java
@@ -7,7 +7,7 @@ public class Scenario {
 
 public static void main(String[] args) {
 	personnages.Gaulois Asterix = new personnages.Gaulois("Asterix", 8);
-	personnages.Romain Minus = new personnages.Romain("Minus",6);
+	personnages.Romain Minus = new personnages.Romain("Minus",30);
 	personnages.Druide panoramix = new personnages.Druide("panoramix",5,10);
 	personnages.Gaulois Obélix =new personnages.Gaulois("Obélix",18);
 	panoramix.parler("je vais aller preparer une petite potion...");
--- src/personnages/Village.java
+++ src/personnages/Village.java
@@ -23,18 +23,51 @@ public class Village {
 	}
 	
 	public void  ajouterHabitant(Gaulois GauloisAAjouter) {
-		if (nbVillageois < nbVillageoisMaximum){
+		if (nbVillageois < villageois.length){
 			villageois[nbVillageois]= GauloisAAjouter;
 			nbVillageois++;
+		}
+		
 			
 		}
 
-	}
+
 	public Gaulois trouverHabitant(int numVillageois) {
 			return villageois[numVillageois];
 			
 		}
+	
+	public void afficherVillageois(){
+		String info="Dans village du chef " + chef.getNom() + " vivent les legendaires gaulois:";
 		
-}
+		System.out.println(info);
+		for( int i =0; i < nbVillageois;i++) {
+			System.out.println(villageois[i].getNom());
+	
+	}
+	}
+
+	
+	public static void main(String[] args){
+		//Gaulois gaulois = village.trouverHabitant(30);
+		Village village = new Village("Village des Irréductibles",30);
+		//Gaulois gaulois = village.trouverHabitant(30)
+		// impossible car l'index 30 depasse le nb villageois maximum
+		
+		Chef chef = new Chef("Abraracourcix",6,village);
+		village.setChef(chef);
+		Gaulois asterix = new Gaulois("asterix",8);
+		village.ajouterHabitant(asterix);
+		//Gaulois gaulois = village.trouverHabitant(1);
+		//System.out.println(gaulois);
+		//La reponse affiché est null car il ya pas de gaulois dans ce village à la position 1
+		Gaulois Obélix =new Gaulois("Obélix",25);
+		village.ajouterHabitant(Obélix);
+		village.afficherVillageois();
+		
+		
+	}
+
+	}
 
 
